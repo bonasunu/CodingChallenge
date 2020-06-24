cc0ecb1c569acdb29f1b4c27dff254161d653e46
bonasunu <bonaventurasunu@gmail.com>
2020-06-24 23:24:11 +0700

    Euler 7 is solved

--- euler-7.js
+++ euler-7.js
@@ -1,16 +1,35 @@
-const nthPrime = (n) => {
-    let number = 1
-    let i = 3
-
-    let prime = false
-    for (let i = 2; i <= Math.sqrt(n); i ++) {
-        if (n % i === 0) {
-            break
+const checkPrime = num => {
+    let prime = true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            prime = false;
+            break;
         }
-        else {prime = true }
-    }
-    console.log(prime)
+    };
+
+    if (num === 1) {
+        prime = false;
+    };
+
+    return prime;
 }
 
-nthPrime(150)
-nthPrime(29)
+//if (checkPrime(13) === true) console.log('Prime');
+
+const nthPrime = n => {
+    let number = 1
+    let countPrime = 0
+    let primeNum = 0
+
+    while (countPrime < n) {
+        if (checkPrime(number) === true) {
+            countPrime++
+            primeNum = number
+        }
+        number++
+    }
+    console.log(primeNum)
+}
+
+nthPrime(100)
+nthPrime(10001)
