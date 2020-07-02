f211a948e054c536a878d44b7b08eb59f2905f17
bonasunu <bonaventurasunu@gmail.com>
2020-07-02 23:54:09 +0700

    implement triangular number

--- euler-12.js
+++ euler-12.js
@@ -1,1 +1,20 @@
+const triangularNumber = n => {
+    let num = 0
+
+    for (let i = 1; i <= n; i++) {
+        num += i
+    }
+
+    return num;
+}
+
+const divisbleTriangleNumber = n => {
+    let result = 0
+
+    while (result === 0) {
+        // call triangularNumber
+        // if divisible n, result change, loop stop
+    }
+    console.log(result)
+}
 
