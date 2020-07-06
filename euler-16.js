418ef23764b9eedbe591443ed7e7ca026e673a1b
bonasunu <bonaventurasunu@gmail.com>
2020-07-06 17:29:51 +0700

    imporve algorithms using BigInt

--- euler-16.js
+++ euler-16.js
@@ -1,10 +1,14 @@
 const powerDigitSum = exp => {
-    let result = Math.pow(2, exp).toString()
+    let result = BigInt(Math.pow(2, exp)).toString()
     let sum = 0;
+
     for (let i = 0; i < result.length; i++) {
-        sum += Number(result[i])
+        sum += parseFloat(result[i])
     }
+    
     console.log(sum)
 }
 
 powerDigitSum(15)
+powerDigitSum(50)
+powerDigitSum(128)
