2a0b0d288f7492a90e7134f1aabd050d930bc813
bonasunu <bonaventurasunu@gmail.com>
2020-07-09 08:04:24 +0700

    trying to solve big numbers problem in euler 20

--- euler-16.js
+++ euler-16.js
@@ -6,6 +6,7 @@ const powerDigitSum = exp => {
         sum += parseFloat(result[i])
     }
     
+    console.log(result)
     console.log(sum)
 }
 
--- euler-20.js
+++ euler-20.js
@@ -3,21 +3,21 @@ const sumFactorialDigits = n => {
     let factorialDigits = 1;
 
     for (let i = n; i > 0; i--) {
-        factorialDigits *= i;
+        factorialDigits *= i
     }
 
-    factorialDigits = BigInt(parseFloat(factorialDigits)).toString();
+    factorialDigits = BigInt(factorialDigits).toString();
 
     for (let i = 0; i < factorialDigits.length; i++) {
         total += Number(factorialDigits[i]);
     }
-
 
     console.log(factorialDigits)
     console.log(total);
 }
 
 sumFactorialDigits(10) // should return 27
-sumFactorialDigits(25)
-sumFactorialDigits(75)
-sumFactorialDigits(100)
+sumFactorialDigits(25) // should return 72
+sumFactorialDigits(75) // should return 432
+sumFactorialDigits(100) // should return 648
+console.log(Math.pow(2, 53))
